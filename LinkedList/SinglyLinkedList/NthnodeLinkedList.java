dc5e95065a5fa8af5559aeee9046f7ec4dc87513
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-08 21:52:20 +0530

    added NthnodeLinkedList.java

---
+++ LinkedList/SinglyLinkedList/NthnodeLinkedList.java
@@ -0,0 +1,73 @@
+package LinkedList.SinglyLinkedList;
+
+import java.util.Scanner;
+
+public class NthnodeLinkedList {
+    Node head;
+    static class Node{
+        int data;
+        Node next;
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    // push
+    public void push(int ndata) {
+        Node newdata = new Node(ndata);
+        if(head==null)
+        {
+            head = new Node(ndata);
+            return;
+        }
+        newdata.next=null;
+        Node n = head;
+        while(n.next!=null)
+        {
+            n=n.next;
+        }
+        n.next = newdata;
+        return;
+    }
+
+    // nth node
+    public int Nth(int ind) {
+        Node temp = head;
+        int count = 0;
+        while(temp!=null)
+        {
+            if(count == ind)
+                return temp.data;
+            count++;
+            temp=temp.next;
+        }
+        assert (false);
+        return 0;
+    }
+
+    // print
+    public void print() {
+        Node n = head;
+        while(n!=null)
+        {
+            System.out.println(n.data+" ");
+            n=n.next;
+        }
+    }
+
+    // main
+    public static void main(String[] args) {
+        NthnodeLinkedList n = new NthnodeLinkedList();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=1;i<num;i++)
+        {
+            n.push(i);
+        }
+        n.print();
+        int node = sc.nextInt();
+        System.out.println("Element --> "+n.Nth(node));
+    }
+}
