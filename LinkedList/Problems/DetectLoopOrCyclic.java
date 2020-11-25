225305816b787fc4b82b52891cc998a458dff1ec
Saravanan raghul <saravananraghul1820@gmail.com>
2020-11-25 15:01:36 +0530

    Problem(2) DetectLoop

---
+++ LinkedList/Problems/DetectLoopOrCyclic.java
@@ -0,0 +1,74 @@
+package LinkedList.Problems;
+
+import java.util.Scanner;
+
+public class DetectLoopOrCyclic {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    // push data
+    public void push(int ndata) {
+        Node newdata = new Node(ndata);
+        if (head == null) {
+            head = new Node(ndata);
+            return;
+        }
+        newdata.next = null;
+        Node n = head;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = newdata;
+        return;
+    }
+
+    // Detect loop -> here two point checking whether each meets
+    public boolean isCyclic() {
+        Node fst = head;
+        Node snd = head;
+        while (fst != null && fst.next != null) {
+            fst = fst.next.next;
+            snd = snd.next;
+            if (fst == snd) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // print
+    public void print() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data + " ");
+            n = n.next;
+        }
+    }
+
+    // main
+    public static void main(String[] args) {
+        DetectLoopOrCyclic Loc = new DetectLoopOrCyclic();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            Loc.push(i);
+        }
+        Loc.print();
+        // Test line for making it in loop
+        // Loc.head.next.next.next.next = Loc.head;
+        if (Loc.isCyclic()) {
+            System.out.println("--> List is Cyclic");
+        } else {
+            System.out.println("--> List is not Cyclic");
+        }
+    }
+}
