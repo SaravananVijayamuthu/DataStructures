6146a96c6de3c1239811262494e8132eb25c5e2f
Saravanan raghul <saravananraghul1820@gmail.com>
2020-11-29 19:41:30 +0530

    Problem(5) -> PrintReverse

---
+++ LinkedList/Problems/PrintReverse.java
@@ -0,0 +1,63 @@
+package LinkedList.Problems;
+
+import java.util.Scanner;
+
+public class PrintReverse {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    // push
+    public void push(int ndata) {
+        Node newdata = new Node(ndata);
+        if (head == null) {
+            head = new Node(ndata);
+            return;
+        }
+        newdata.next = null;
+        Node n = head;
+        while (n.next != null) {
+            n = n.next;
+        }
+        n.next = newdata;
+        return;
+    }
+
+    // reverse
+    public void reverse(Node head) {
+        if (head == null)
+            return;
+        reverse(head.next);
+        System.out.println(head.data + " ");
+    }
+
+    // print
+    public void print() {
+        Node n = head;
+        while (n != null) {
+            System.out.println(n.data + " ");
+            n = n.next;
+        }
+    }
+
+    // main
+    public static void main(String[] args) {
+        PrintReverse PR = new PrintReverse();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int var = sc.nextInt();
+            PR.push(var);
+        }
+        System.out.println("Reversed Linked List");
+        PR.reverse(head);
+    }
+}
