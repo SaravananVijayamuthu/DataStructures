539ce89a97b2fd202ff773359f3a148436903d14
Saravanan raghul <saravananraghul1820@gmail.com>
2021-01-11 13:07:01 +0530

    String - 2 -> PlaindromeString DESC

--- Companies/Zoho/Strings/PalindromeString.java
+++ Companies/Zoho/Strings/PalindromeString.java
@@ -3,8 +3,27 @@ package Companies.Zoho.Strings;
 import java.util.Scanner;
 
 /**
- * PalindromeString
+PalindromeString
+
+Given a string S, check if it is palindrome or not.
+
+Example 1:
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+
+Example 2:
+Input: S = "abc" 
+Output: 0
+Explanation: S is not a palindrome
+
+Expected Time Complexity: O(Length of S) 
+Expected Auxiliary Space: O(1) 
+
+Constraints:
+1 <= Length of S <= 105 
  */
+
 public class PalindromeString {
 
     public static void main(String[] args) {
